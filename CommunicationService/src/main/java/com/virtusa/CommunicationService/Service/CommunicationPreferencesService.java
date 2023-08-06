8e0ca73117049e65018f154f044130fe465cf01d
imsurajmourya <imsurajmourya@gmail.com>
2023-08-06 08:10:40 +0000

    Communication Service

---
+++ CommunicationService/src/main/java/com/virtusa/CommunicationService/Service/CommunicationPreferencesService.java
@@ -0,0 +1,16 @@
+package com.virtusa.CommunicationService.Service;
+import com.virtusa.CommunicationService.model.CommunicationPreferences;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CommunicationPreferencesService {
+
+
+    public CommunicationPreferences updateCommunicationPreferences(Long patientId, CommunicationPreferences communicationPreferences) {
+    }
+
+    public CommunicationPreferences getCommunicationPreferences(Long patientId) {
+    }
+}
+
---
+++ CommunicationService/src/main/java/com/virtusa/CommunicationService/model/CommunicationPreferences.java
@@ -0,0 +1,41 @@
+package com.virtusa.CommunicationService.model;
+
+public class CommunicationPreferences {
+    private Long patientId;
+    private String communicationMethod;
+    private String phoneNumber;
+    private String email;
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getCommunicationMethod() {
+        return communicationMethod;
+    }
+
+    public void setCommunicationMethod(String communicationMethod) {
+        this.communicationMethod = communicationMethod;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
